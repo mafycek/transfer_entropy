a7735988bc95bf9320452450b27ecb75d28daadf
mafycek <h.lavicka@email.cz>
2023-06-14 12:40:26 +0200

    Improvement of structure of script generators

--- test/qsub_script_generator.py
+++ test/qsub_script_generator.py
@@ -17,6 +17,12 @@ if __name__ == "__main__":
     sql_password = os.getenv("SQL_PASSWORD")
     sql_database = os.getenv("SQL_DATABASE")
 
+    history_first = "0 , 0 1 , 0 1 2 3"
+    history_second = "0 , 0 1 , 0 1 2 3"
+    alpha_params = "0.1 3.0 117"
+    dataset_1_selector = "4 5 6 7"
+    dataset_2_selector = "4 5 6 7"
+
     dbHandler = FinanceMongoDatabasePlugin(
         url,
         table,
@@ -45,6 +51,6 @@ if __name__ == "__main__":
     for code in list_of_codes[1:500]:
         with open(directory + f"script_{base_code}_{code}.sh", "wt") as fh:
             print(
-                f"./calculations/transfer_entropy.sif --database=True --database_nosql_url={url} --database_sql_url={url} --database_sql_username={sql_username} --database_sql_password={sql_password} --database_sql_table={sql_database} --database_nosql_username={username} --database_nosql_password={password} --database_nosql_table=t{table} --dataset_1_code={base_code} --dataset_2_code={code} --dataset_1_selector 4 5 6 7 --dataset_2_selector 4 5 6 7 --alpha_params 0.1 3.0 117 --history_first 0 , 0 1 , 0 1 2 3  --history_second 0 , 0 1 , 0 1 2 3 --maximal_neighborhood {maximal_neighborhood}",
+                f"./calculations/transfer_entropy.sif --database=True --database_nosql_url={url} --database_sql_url={url} --database_sql_username={sql_username} --database_sql_password={sql_password} --database_sql_table={sql_database} --database_nosql_username={username} --database_nosql_password={password} --database_nosql_table={table} --dataset_1_code={base_code} --dataset_2_code={code} --dataset_1_selector {dataset_1_selector} --dataset_2_selector {dataset_2_selector} --alpha_params {alpha_params} --history_first {history_first} --history_second {history_second} --maximal_neighborhood {maximal_neighborhood}",
                 file=fh,
             )
